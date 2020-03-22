86ed96ee26a8dd075165609dc0797196455038a8
Eduardo Burgos <eburgos@gmail.com>
2020-03-22 18:47:51 -0400

    add eslint fix some features

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  ignorePatterns: ["temp.js", "node_modules/", "generated/"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: `./tsconfig.json`
+  },
+  plugins: ["@typescript-eslint", "eslint-plugin-prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier"
+  ],
+  rules: {
+    "prettier/prettier": ["warn", { trailingComma: "none" }],
+    "no-undef": 0,
+    "id-length": [
+      "error",
+      { min: 3, properties: "never", exceptions: ["_", "id", "x", "y"] }
+    ],
+    "@typescript-eslint/indent": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-parameter-properties": 0,
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "_.*" }
+    ],
+    "@typescript-eslint/array-type": [
+      "error",
+      {
+        default: "array-simple"
+      }
+    ],
+    "@typescript-eslint/ban-ts-ignore": ["error"],
+    "@typescript-eslint/no-floating-promises": ["error"],
+    "@typescript-eslint/no-inferrable-types": ["error"],
+    "@typescript-eslint/no-magic-numbers": 0,
+    "@typescript-eslint/no-this-alias": ["error"],
+    "@typescript-eslint/prefer-string-starts-ends-with": ["error"]
+  }
+};
--- index.ts
+++ index.ts
@@ -5,32 +5,35 @@ export interface BaseConfig {
 }
 
 export interface LongUnitDescription {
+  features?: { [feature: string]: any };
   id: string;
   version: string;
 }
 
 export type UnitDescription = "string" | LongUnitDescription;
 
-export interface UnitDefinition <C extends BaseConfig, U> extends LongUnitDescription {
+export interface UnitDefinition<C extends BaseConfig, U>
+  extends LongUnitDescription {
   config: C | (() => Promise<C>);
-  start:(
-    config: C,
-    getUnit: <X>(unitId: string) => X
-  ) => Promise<U>;
-  stop: () => Promise<void>;
+  start: (config: C, getUnit: <X>(unitId: string) => X) => Promise<U>;
+  stop: (getUnit: <X>(unitId: string) => X) => Promise<void>;
 }
 
 interface UnitInstance extends UnitDefinition<any, any> {
   state: "starting" | "started" | "stopped";
-  dependencies: { [id: string]: UnitInstance };
+  dependencies: LongUnitDescription[];
+  dependencyInstances: { [id: string]: UnitInstance };
   value: any;
 }
 
 const registry: { [id: string]: UnitInstance } = {};
 
-export function define<C extends BaseConfig, U>(consumes: UnitDescription[], unit: UnitDefinition<C, U>) {
+export function define<C extends BaseConfig, U>(
+  consumes: UnitDescription[],
+  unit: UnitDefinition<C, U>
+) {
   const normalizedDependencies: LongUnitDescription[] = (consumes || []).map(
-    function(item) {
+    function (item) {
       const normalizedItem =
         typeof item === "string"
           ? { id: item, version: "*" }
@@ -42,49 +45,80 @@ export function define<C extends BaseConfig, U>(consumes: UnitDescription[], uni
     }
   );
 
-    if (registry[unit.id]) {
-      throw new Error(`Unit "${unit.id}" already defined`);
-    }
-    const unitInstance: UnitInstance = {
-      ...unit,
-      value: undefined,
-      state: "stopped",
-      dependencies: normalizedDependencies.reduce((acc: {[id: string]: UnitInstance }, next) => {
-        acc[next.id] = registry[next.id];
-        if (!acc[next.id]) {
-          throw new Error(`Unit "${unit.id}" has missing dependency "${next.id}"`);
-        }
-        return acc;
-      }, {})
-    };
-    registry[unit.id] = unitInstance;
+  if (registry[unit.id]) {
+    throw new Error(`Unit "${unit.id}" already defined`);
+  }
+  const unitInstance: UnitInstance = {
+    ...unit,
+    value: undefined,
+    state: "stopped",
+    dependencies: normalizedDependencies,
+    dependencyInstances: {}
+  };
+  registry[unit.id] = unitInstance;
 }
 
-async function initOne(one: UnitInstance, queued: {[name: string]: boolean }): Promise<void> {
-  const stopped = Object.values(one.dependencies).filter(dep => dep.state === "stopped");
-  stopped.forEach(dep => { if (queued[dep.id]) { throw new Error(`Circular reference at "${dep.id}"`); }});
+async function initOne(
+  one: UnitInstance,
+  queued: { [name: string]: boolean }
+): Promise<void> {
+  const stopped = Object.values(one.dependencyInstances).filter(
+    (dep) => dep.state === "stopped"
+  );
+  stopped.forEach((dep) => {
+    if (queued[dep.id]) {
+      throw new Error(`Circular reference at "${dep.id}"`);
+    }
+  });
 
   await stopped.reduce(async (acc, next) => {
     await acc;
-    await initOne(next, {...queued, [next.id]: true });
+    await initOne(next, { ...queued, [next.id]: true });
   }, Promise.resolve());
 
   const getUnit = (unitId: string): any => {
-    if (one.dependencies[unitId]) {
-      return one.dependencies[unitId];
+    if (one.dependencyInstances[unitId]) {
+      return one.dependencyInstances[unitId];
     }
-    throw new Error(`Unit "${one.id}" cannot reference "${unitId}" because it does not depend on it.`);
+    throw new Error(
+      `Unit "${one.id}" cannot reference "${unitId}" because it does not depend on it.`
+    );
   };
 
+  one.dependencies.reduce((acc: { [id: string]: UnitInstance }, next) => {
+    acc[next.id] = registry[next.id];
+    if (!acc[next.id]) {
+      throw new Error(`Unit "${one.id}" has missing dependency "${next.id}"`);
+    }
+    const nextFeatures = next.features;
+    if (typeof nextFeatures === "object") {
+      Object.keys(nextFeatures).forEach((feature) => {
+        const features = acc[next.id].features;
+        if (typeof features !== "object") {
+          throw new Error(
+            `Unit "${one.id}" has a dependency "${next.id}" that needs a non-implemented feature "${feature}"`
+          );
+        }
+        if (features[feature] !== nextFeatures[feature]) {
+          throw new Error(
+            `Unit "${one.id}" has a dependency "${next.id}" that requires feature "${feature}" = "${nextFeatures[feature]}" but the provider is "${features[feature]}"`
+          );
+        }
+      });
+    }
+    return acc;
+  }, one.dependencyInstances);
+
   one.state = "starting";
-  const config = typeof one.config === "function"? (await one.config()) : one.config;
+  const config =
+    typeof one.config === "function" ? await one.config() : one.config;
   one.value = await one.start(config, getUnit);
   one.state = "started";
 }
 
 export function init(): Promise<void> {
   const stopped = Object.values(registry).filter(
-    unit => unit.state === "stopped"
+    (unit) => unit.state === "stopped"
   );
   return stopped.reduce(async (acc, next) => {
     await acc;
